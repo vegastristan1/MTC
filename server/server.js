c1c34abe81dc04fb6549655a93fa9303f185e4e5
Tristan <vegastristan1@gmail.com>
2025-10-17 15:45:29 +0800

    removed user link

--- server/server.js
+++ server/server.js
@@ -7,17 +7,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/users', async (req, res) => {
-  try {
-    await sql.connect(config);
-    const result = await sql.query`SELECT * FROM Users`;
-    res.json(result.recordset);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Database query failed');
-  }
-});
-
 // Test API route
 app.get('/api/SorMaster', async (req, res) => {
     try {
